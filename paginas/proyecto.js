e4ec9645a7976da79d45f521bf13bec4fea464ab
silfigue <spfigue@gmail.com>
2023-09-14 20:49:01 -0300

    arreglando archivo

--- paginas/proyecto.js
+++ paginas/proyecto.js
@@ -1,12 +1,14 @@
 //donaciones de visitantes de la página web
 
 let contribución = prompt ("Tu contribución puede hacer la diferencia. ¿Nos Ayudas?")
-if (contribución == si) 
-let montoContribución = prompt ("Ingrese el monto de su aporte")
-alert ("el importe a donar es $+ montoContribución")
-let nombreYApellido = prompt ("ingrese su nombre y apellido")
-alert ("su nombre y apellido es"+  nombreYApellido)
-let destino = prompt ("Ingrese a dónde destina su aporte: FAUNA NATIVA - FLORA NATIVA - AMBIENTE")
+if (contribución == si){
+    let montoContribución = prompt ("Ingrese el monto de su aporte")
+    alert ("el importe a donar es $"+ montoContribución)
+    let nombreYApellido = prompt ("ingrese su nombre y apellido")
+    alert ("su nombre y apellido es"+  nombreYApellido)
+    let destino = prompt ("Ingrese a dónde destina su aporte: FAUNA NATIVA - FLORA NATIVA - AMBIENTE") 
+}
+
 
 //Si ingresa FAUNA NATIVA
 
