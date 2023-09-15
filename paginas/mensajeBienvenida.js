50811e047480279f7860ae1a649b97249dd49a14
pao-a <142278663+pao-a@users.noreply.github.com>
2023-09-14 21:10:15 -0300

    Add files via upload

---
+++ paginas/mensajeBienvenida.js
@@ -0,0 +1,8 @@
+
+function mensajeDeBienvenida (){
+let nombre = prompt ("¿Cómo podemos llamarte?");
+alert ("Muchas gracias por tu visita " + nombre);
+console.log ("Muchas gracias por tu visita" + nombre);
+
+}
+mensajeDeBienvenida ();
