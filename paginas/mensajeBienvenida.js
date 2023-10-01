540133dc7511308f8e97fbb42f2076cead3918a5
pao-a <142278663+pao-a@users.noreply.github.com>
2023-10-01 15:24:49 -0300

    Borre paginas/mensajeBienvenida.js

--- paginas/mensajeBienvenida.js
+++
@@ -1,8 +0,0 @@
-
-function mensajeDeBienvenida (){
-let nombre = prompt ("¿Cómo podemos llamarte?");
-alert ("Muchas gracias por tu visita " + nombre);
-console.log ("Muchas gracias por tu visita" + nombre);
-
-}
-mensajeDeBienvenida ();
