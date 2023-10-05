79744f868b8b5fb4781090c9e08e4a87069d3776
sanmargo <marcela80716@hotmail.com>
2023-10-05 20:09:35 -0300

    carrito

--- paginas/carrito.js
+++ paginas/carrito.js
@@ -1,3 +1,76 @@
+let jsonDeProductos = `[
+{
+    "titulo": "Gorra de plantas",
+    "precio": "$12.999,99",
+    "imagen": "https://images-na.ssl-images-amazon.com/images/I/61UJKi-JA1L._AC_UX679_.jpg"
+ },
+{
+    "titulo": "Gorra de animales",
+    "precio": "$12.999,99",
+    "imagen": "https://images-na.ssl-images-amazon.com/images/I/712MlOTAQmL._AC_UL1200_.jpg"
+},
+{
+    "titulo": "Remera de plantas",
+    "precio": "$32.999,99",
+    "imagen": "https://d22fxaf9t8d39k.cloudfront.net/3d83025f463bca161f25fd17d7d6cee7fb1fdeedd142dfbda4442db299e836691881.jpeg"
+
+},
+{
+    "titulo": "Remera de animales",
+    "precio": "$32.999,99",
+    "imagen": "https://i.pinimg.com/originals/c1/e5/f6/c1e5f697936bab213cb4b612b443699b.jpg"
+}
+]`
+
+
+let jsonConvertido = JSON.parse(jsonDeProductos)
+for (let i = 0; i <jsonConvertido.length; i++){
+
+    
+let article = document.createElement("article")
+
+let h3 = document.createElement("h3")
+h3.innerText = jsonConvertido[i].titulo
+
+let img = document.createElement("img")
+img.style.width = "45%"
+img.src = jsonConvertido[i].imagen
+
+let p = document.createElement("p")
+p.innerText = jsonConvertido[i].precio
+
+
+let boton = document.createElement("button")
+boton.innerText = "Agregar al carrito"
+
+article.appendChild(h3)
+article.appendChild(img)
+article.appendChild(p)
+article.appendChild(boton)
+
+document.getElementById("productos").appendChild(article)
+}
+
+let sectioncarrito = document.getElementById ("carrito")
+sectioncarrito.addEventListener ("focus", () => {
+sectioncarrito.style.border = "2px solid red"
+}
+)
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  //const Usuario = {
     //nombre:"Paula",
     //dni:"34804013",
@@ -69,42 +142,6 @@
 
 
  
- let arrayDeProductos=[{
-    titulo:"Gorra de plantas",
-    precio:12.999,
-    imagen:"https://ae01.alicdn.com/kf/S1a5a9a9c7b474e358ee61896a038491d5/Gorra-de-verano-para-mujer-sombrero-de-sol-con-estampado-de-hojas-azules-y-plantas-tropicales.jpg",
- },
-{
-    titulo:"Gorra de animal",
-    precio:12.999,
-    imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS37sfGu5QFZSlmyCN12M7lZ3rxar5C1nLaVQ&usqp=CAU",
-},
-{
-    titulo:"Remera de plantas",
-    precio:12.999,
-    imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUR_tbJehGDAD3AVsclPXlifXHXwJZNGeSqM5BAc1ZpHs4JZjX_18QK7xt_8cStU5ysGc&usqp=",
 
-},
-{
-    titulo:"Remera de animales",
-    precio:12.999,
-    imagen:"https://http2.mlstatic.com/D_NQ_NP_655243-MLA41121980841_032020-O.webp",
-},
-]
 
-for(let i=0;i < arrayDeProductos.length;i++){
-    let article = document.createElement("article")
-    let imagen = document.createElement("img")
-    let titulo = document.createElement("h3")
-    let precio=document.createElement("p")
 
-    imagen.src=arrayDeProductos[i].tituloprecio.innerText="$"+ arrayDeProductos[i].precio
-
-    article.appendChild(imagen)
-    article.appendChild(titulo)
-    article.appendChild(precio)
-
-    document.querySelector("section.listado").appendChild(article)
-}
-
-                        
